c4c591954142234102f1e11078f90ac0b4f84cdd
Nikita Shapovalov <a191a191a@gmail.com>
2023-09-21 19:03:18 +0300

    connected to database

--- main.go
+++ main.go
@@ -1,27 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"github.com/lib/pq"
+	_ "github.com/lib/pq" // Import the PostgreSQL driver
 	"go-cqrs/interfaces/web"
 	"go-cqrs/internal/app/customer"
 	"go-cqrs/internal/app/order"
-	"database/sql"
-	"fmt"
-	_ "github.com/lib/pq" // Import the PostgreSQL driver
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 )
 
+var (
+	db  *gorm.DB
+	err error
+)
+
 func main() {
-	// Connect to PostgreSQL database
-	db, err := sql.Open("postgres", "user=your_user dbname=your_db_name sslmode=disable")
+	// Initialize the database connection
+	//dsn := "host=localhost user=postgres password=password dbname=postgres port=5432 sslmode=disable"
+	//dsn := "host='vultr-prod-5f785376-9e78-4398-86ef-5bd59e46afa8-vultr-prod-5c15.vultrdb.com' user=vultradmin password='AVNS_4ijKKcYd-4-mdo65XBT' dbname=defaultdb port=16751 sslmode=disable"
+	connectionString := "postgres://vultradmin:AVNS_4ijKKcYd-4-mdo65XBT@vultr-prod-5f785376-9e78-4398-86ef-5bd59e46afa8-vultr-prod-5c15.vultrdb.com:16751/defaultdb"
+	db, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to open database connection:", err)
+	}
+
+	// Ping the database to check connectivity
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get the underlying SQL database", err)
+	}
+
+	// If Ping() succeeds, you have a valid database connection
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatal("Database connection failed:", err)
+	}
+	fmt.Println("Database connection established successfully")
+
+	print("here")
+	defer sqlDB.Close()
+
+	dbname := "core-service-db"
+	// Check if the database already exists
+	var dbExists bool
+	err = sqlDB.QueryRow("SELECT EXISTS(SELECT datname FROM pg_database WHERE datname=$1)", dbname).Scan(&dbExists)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+
+	if !dbExists {
+		// Database does not exist, so create it
+		err := db.Exec("CREATE DATABASE " + pq.QuoteIdentifier(dbname))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Database '%s' created.\n", dbname)
+	} else {
+		fmt.Printf("Database '%s' already exists.\n", dbname)
+	}
 
 	// Initialize the order and customer services with the database connection
-	orderService := order.NewService(db)
-	customerService := customer.NewService(db)
+	orderService := order.NewService(sqlDB)
+	customerService := customer.NewService(sqlDB)
 
 	// Configure routes
 	router := web.SetupRouter(*orderService, *customerService)
