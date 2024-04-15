097d30270b33bc80ba1e82e8e404bf9c68a2c63e
Nikita S. <a191a191a@gmail.com>
2024-04-15 09:23:45 +0400

    cleanup

--- internal/app/order/order_controller.go
+++ internal/app/order/order_controller.go
@@ -36,35 +36,6 @@ func (c *Controller) CreateOrderHandler(w http.ResponseWriter, r *http.Request) 
 	HandleSuccessResponse(w, fmt.Sprintf("order with id: %d created", orderId))
 }
 
-//func (c *Controller) GetOrderHandler(w http.ResponseWriter, r *http.Request) {
-//	vars := mux.Vars(r)
-//	orderIDStr, ok := vars["id"]
-//	if !ok {
-//		HandleErrorResponse(w, fmt.Errorf("order ID not found in URL"))
-//		return
-//	}
-//
-//	orderID, err := strconv.Atoi(orderIDStr)
-//	if err != nil {
-//		HandleErrorResponse(w, fmt.Errorf("invalid order ID: %s", orderIDStr))
-//		return
-//	}
-//
-//	getQuery := query_handlers.GetOrderQuery{ID: orderID}
-//	order, err := c.queryHandler.HandleGetOrderQuery(r.Context(), getQuery)
-//	if err != nil {
-//		HandleErrorResponse(w, err)
-//		return
-//	}
-//
-//	orderJSON, err := json.Marshal(order)
-//	if err != nil {
-//		HandleErrorResponse(w, err)
-//		return
-//	}
-//	HandleSuccessResponse(w, string(orderJSON))
-//}
-
 func (c *Controller) GetOrderHandler(w http.ResponseWriter, r *http.Request) {
 	orderIDStr := mux.Vars(r)["id"]
 	orderID, err := strconv.Atoi(orderIDStr)
@@ -79,7 +50,6 @@ func (c *Controller) GetOrderHandler(w http.ResponseWriter, r *http.Request) {
 		HandleErrorResponse(w, err)
 		return
 	}
-
 	HandleSuccessResponse(w, order)
 }
 
@@ -128,17 +98,12 @@ func (c *Controller) UpdateOrderHandler(w http.ResponseWriter, r *http.Request) 
 	}
 	defer r.Body.Close()
 
-	// Set the ID in the update command
 	updateCmd.ID = orderID
-
-	// Call the command handler to update the order
 	err = c.commandHandler.HandleUpdateOrderCommand(r.Context(), updateCmd)
 	if err != nil {
 		HandleErrorResponse(w, err)
 		return
 	}
-
-	// Respond with a success message
 	HandleSuccessResponse(w, fmt.Sprintf("Order ID:%d has been successfully updated", orderID))
 }
 
@@ -146,27 +111,19 @@ type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
-type SuccessResponse struct {
-	Message string `json:"message"`
-}
-
 func HandleErrorResponse(w http.ResponseWriter, errorMessage error) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusBadRequest)
 
-	// Create and marshal the error response
 	response := ErrorResponse{Error: errorMessage.Error()}
 	jsonResponse, err := json.Marshal(response)
 	if err != nil {
-		// Handle JSON marshaling error
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// Send the JSON response
 	_, err = w.Write(jsonResponse)
 	if err != nil {
-		// Handle write error
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -176,19 +133,14 @@ func HandleSuccessResponse(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	// Create and marshal the success response
-	//response := SuccessResponse{Message: message}
 	jsonResponse, err := json.Marshal(data)
 	if err != nil {
-		// Handle JSON marshaling error
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// Send the JSON response
 	_, err = w.Write(jsonResponse)
 	if err != nil {
-		// Handle write error
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
